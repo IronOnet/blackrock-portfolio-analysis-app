970fca3d24936ac36c1def3547b81c2330421072
Rushil Kapadia <rushilkapadia@calvisitor-10-105-152-83.calvisitor.1918.berkeley.edu>
2018-11-04 04:25:32 -0800

    ATHARVA U MEEEEEEMMMEEEE

--- blackrock.py
+++ blackrock.py
@@ -1,23 +1,28 @@
 from newsapi import NewsApiClient
 import datetime
+from datetime import timedelta
 from data import get_stats, time_convert
 
 # import pandas as pd
 # data = pd.read_csv("companylist.csv")
 
+def numArt (time, company):
+    newsapi = NewsApiClient(api_key='69f7c79fd92140f19221a257ec5e980c')
+    articles = newsapi.get_everything(q=company,from_param=str(time),to=str(time))
+    return (articles['totalResults'])
+ 
+
+
+
 def companyCount(time, company):
     newsapi = NewsApiClient(api_key='69f7c79fd92140f19221a257ec5e980c')
-
+    print()
     # companies = list(data['Symbol'])
     countList = []
     statsList = []
     count = 0
-
-    for i in range(0, 90, 3):
-        articles = newsapi.get_everything(q=company,from_param=str(time+datetime.timedelta(days=i)),to=str(time+datetime.timedelta(days=i)))
-        for j in articles:
-            count+=1
-        countList.append(count)
-        statsList.append(get_stats(company, time_convert(time+(datetime.timedelta(days=i+7)))))
+    for i in range(18, 28, 1):
+        countList.append(numArt(time+datetime.timedelta(days= -1*i), company))
+        statsList.append(get_stats(company, time_convert(time+(datetime.timedelta(days= -i + 7)))))
     return (countList, statsList)
-print(companyCount(datetime.date.today() - (datetime.timedelta(days=30)), 'GOOGL'))
+print(companyCount(datetime.date.today(), 'GOOGL'))
