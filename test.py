4c481201fb879abcc06727d4bfd3c3169f0e4fba
Rushil Kapadia <rushilkapadia@calvisitor-10-105-152-83.calvisitor.1918.berkeley.edu>
2018-11-03 21:35:28 -0700

    YUH

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 import requests
 import json
 
-portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/performance", params= {'identifiers':"IXN"})
+portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/security-data", params= {'identifiers':"IXN"})
 dic_data = json.loads(portfolioAnalysisRequest.text) #string json 
 
 result = dic_data['resultMap'] # dictionary of dic_data
 
+values = (result['SECURITY'][0])
+
+for v in values:
+	print(v," : ", values[v])
 # below lines are to access the 0th item in the list and view its values, comment out if necessary
 #values = result['RETURNS']
 #dvalues = values[0]
