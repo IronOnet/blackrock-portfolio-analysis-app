367993d7c2df09abe4dfcdfc410244d0865b4285
Rushil Kapadia <rushilkapadia@calvisitor-10-105-152-83.calvisitor.1918.berkeley.edu>
2018-11-04 01:23:04 -0800

    HELL YAAAA

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from datetime import date
 
 
-
+# Takes in company and date and returns the stats for one day
 def one_day_stats (company, date):
 
 	portfolioAnalysisRequest = requests.get("https://www.blackrock.com/tools/hackathon/performance", params= {'identifiers': company, 'startDate' : date, 'endDate' : date})
@@ -14,18 +14,18 @@ def one_day_stats (company, date):
 	b = dic_data['resultMap']['RETURNS'][0] # enter data for the 
 
 	
-	for a in b:
-		if (a == "latestPerf"):
-			return b[a]['oneDay']
+
+	return b.get("latestPerf")['oneDay']
 
 
-
+# Takes in company and date and returns the stats for one week
 def one_week_stats (company, curr_date):
 	output = 1
 	for i in range(7):
 		output *= (1 + one_day_stats(company, time_decrement(curr_date, i)))
 	return output - 1
 
+# Takes in API date format and number of days before and returns API date format
 def time_decrement(time, i):
 	time = int(time)
 	delta = timedelta(days=-i)
@@ -36,13 +36,14 @@ def time_decrement(time, i):
 	year = time
 	return time_convert((date(year,month,day) + delta))
 
-
+# Converts from datetime date to API date form
 def time_convert(date):
 	return str((date.year*10000) + (date.month*100) + (date.day))
 
-
-
-#one_day_stats("GS", '20161212')
+# Today's date
 today = date.today()
-yesterday = date.today()
-print(one_week_stats('GS',time_convert(today)))
+
+# Main function to run, takes a company and optionally the date and returns the seven day stats
+def get_stats(company,cdate=time_convert(today)):
+	return ("%.3f" % one_week_stats(company,cdate))
+	
